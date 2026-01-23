8a5493c0f3b3605fcf3907c99b622da644b94e78
Eleonora Petzold <eleonora.petzold@uni-jena.de>
2026-01-23 11:23:51 +0100

    make Quality-tab disable for Tombstone page https://github.com/bexis/BExIS-intern/issues/767

--- BExIS.Modules.DQM.UI/Hooks/DataQualityHook.cs
+++ BExIS.Modules.DQM.UI/Hooks/DataQualityHook.cs
@@ -32,6 +32,12 @@ namespace BExIS.Modules.Dqm.UI.Hooks
             // if one fail then access is denied
             if (hasAccess == false || hasRights == false) Status = HookStatus.AccessDenied;
             else Status = HookStatus.Open;
+
+            using (var datasetManager = new DatasetManager())
+            {
+                var dataset = datasetManager.GetDataset(id);
+                if (dataset.Status != Dlm.Entities.Data.DatasetStatus.CheckedIn) Status = HookStatus.Disabled;
+            }
         }
 
         private void checkAvailablity(long id)
